b3f5d428ceda8d605f2213e87711a24f13ab6128
samir-s-hassan <hsamir347@gmail.com>
2024-03-23 12:56:36 -0400

    created a custom table view cell and added its outlets into the class

---
+++ ios101-project5-tumblr/TumCell.swift
@@ -0,0 +1,29 @@
+//
+//  TumCell.swift
+//  ios101-project5-tumblr
+//
+//  Created by Samir Hassan on 3/23/24.
+//
+
+import UIKit
+
+class TumCell: UITableViewCell {
+
+    @IBOutlet weak var tumblrPicView: UIImageView!
+    
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    @IBOutlet weak var summaryLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
