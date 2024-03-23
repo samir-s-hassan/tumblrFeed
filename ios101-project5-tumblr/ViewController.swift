e81705f9368afcb31206d08bfa32ddc06811ee56
samir-s-hassan <hsamir347@gmail.com>
2024-03-23 13:03:14 -0400

    working on displaying the posts data into our views

--- ios101-project5-tumblr/ViewController.swift
+++ ios101-project5-tumblr/ViewController.swift
@@ -9,7 +9,8 @@ import Nuke
 //Samir - conform to the protocol is to implement a few methods that the data source needs to display the table view
 class ViewController: UIViewController, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 50 //50 rows
+        // return the number of posts we'll be displaying        
+        return posts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -30,6 +31,10 @@ class ViewController: UIViewController, UITableViewDataSource {
     @IBOutlet weak var tableView: UITableView!
     
     
+    // A property to store the blogs we fetch.
+    private var posts: [Post] = []
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
